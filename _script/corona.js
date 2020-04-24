9ed01e45dee462a18306037d0e6fb538aeb2c90f
birsenek <birsenek@gmail.com>
2020-04-24 17:08:07 -0300

    dados atualizados, estilo refeito sem boostrap

--- _script/corona.js
+++ _script/corona.js
@@ -1,14 +1,17 @@
 function estimateDeaths() {
         let totalPopulation = document.getElementById('tamPop').value
-        let infectionRate = document.getElementById('txInfec').value
-        let deathRate = document.getElementById('txMort').value
+        let infectionRate = document.getElementById('txInfec-slider').value
+        let deathRate = document.getElementById('txMort-slider').value
         let totMortos = (totalPopulation * (infectionRate/100) * (deathRate/100))
+        if (totMortos > 1) {
+            totMortos = totMortos.toFixed(4)
+        }
         resultMort.innerHTML = `A estimativa é de ${totMortos} milhões de mortos.`      
 }
 
 function slider() {
-    this.infectionRate = document.getElementById('txInfec').value
+    this.infectionRate = document.getElementById('txInfec-slider').value
     infectionRates.innerHTML = `${infectionRate}%`
-    this.deathRate = document.getElementById('txMort').value
+    this.deathRate = document.getElementById('txMort-slider').value
     deathRates.innerHTML = `${deathRate}%`
 }
